ff92e0990f341eba33716f21374351f1170e4818
Damaris Fernandez <helloworld963369@gmail.com>
2025-05-20 23:59:22 -0300

    Configurando prisma

--- src/server.js
+++ src/server.js
@@ -6,12 +6,13 @@ import maeSolo from "./routes/api.routes.js";
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
 
 app.use(logger);
 app.use(express.json());
 
 app.use("/maeSolo", maeSolo);
+
+const PORT = process.env.PORT || 8800;
 
 app.listen(PORT, async () => {
     logEvents(`Servidor rodando na porta ${PORT}`, "listen.log");
